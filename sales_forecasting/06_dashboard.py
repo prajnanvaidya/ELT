5d09f52698de807d016a9e196a7aa44a11f4e6ad
Prajnan Vaidya <prajnan247@example.com>
2026-07-02 00:35:51 +0530

    sales_forecast-06

---
+++ sales_forecasting/06_dashboard.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+from pyspark.sql import functions as F
+
+--------------
+
+SOURCE_TABLE = "retailmart.ml_db.future_sales_forecast"
+
+OUTPUT_TABLE = "retailmart.ml_db.sales_forecast_dashboard"
+
+-----------------
+
